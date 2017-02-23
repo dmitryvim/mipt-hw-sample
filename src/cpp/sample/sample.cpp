f738fb97021375a168ac322e2e96347938a344eb
antonsold <soldatenkovanton@mail.ru>
2017-02-23 20:54:00 +0300

    Ya vypolnil zadanie 1

--- src/cpp/sample/sample.cpp
+++ src/cpp/sample/sample.cpp
@@ -7,3 +7,13 @@ int main() {
     std::cin >> n;
     std::cout << factorial(n);
 }
+
+int factorial(int n)
+{
+    int result = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
